58318ad41e68ab2c775a710d313de957e7df2b97
albee <bongio.alex@gmail.com>
2015-08-15 20:12:08 -0400

    Fixed the knockback vector for the sword

--- weapons/sword.c
+++ weapons/sword.c
@@ -180,7 +180,7 @@ static void collideWithSword(Weapon *self, void *o, int collCode, CollisionType 
         printf("Hit something\n");
         Enemy *enemy = (Enemy *)o;
         enemy->takeDamage(enemy, DAMAGE);
-        addExternalMove(&enemy->e, self->e.x, self->e.y, 20, 0.25);
+        addExternalMove(&enemy->e, enemy->e.x - self->e.x, enemy->e.y - self->e.y, 20, 1);
         self->e.active = 0;
         self->cancelled = 1;
     }
