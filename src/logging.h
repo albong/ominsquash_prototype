59d2878eae9caad7c22f99041dfea9bad4388e67
albong <bongio.alex@gmail.com>
2018-12-24 13:19:53 -0500

    Added new logging level.

--- src/logging.h
+++ src/logging.h
@@ -3,21 +3,28 @@
 
 #include <stdio.h>
 
-//Level 0: log info, warnings, and errors
-//Level 1: log warnings and errors
-//Level 2: log errors
+//Level 0: log debug, info, warnings, and errors
+//Level 1: log info, warnings, and errors
+//Level 2: log warnings and errors
+//Level 3: log errors
 #define LOGGING_LEVEL 0
 
-//INF=INFO, WAR=WARNING, ERR=ERROR.  I have a thing about equal lengths.
+//DEB=DEBUG, INF=INFO, WAR=WARNING, ERR=ERROR.  I have a thing about equal lengths.
 //the '##' before __VA_ARGS__ does some token nonsense that makes things work if there are no arguments
 //removes the prior comma essentially.  M A G I C
 #if LOGGING_LEVEL == 0
+    #define LOG_DEB(fmt, ...) printf("DEBUG %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+    #define LOG_DEB(fmt, ...)
+#endif
+
+#if LOGGING_LEVEL <= 1
     #define LOG_INF(fmt, ...) printf("INFO %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
     #define LOG_INF(fmt, ...)
 #endif
 
-#if LOGGING_LEVEL <= 1
+#if LOGGING_LEVEL <= 2
     #define LOG_WAR(fmt, ...) printf("WARNING %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
     #define LOG_WAR(fmt, ...)
